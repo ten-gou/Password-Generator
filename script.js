98c2076687d5963e82986f11bfc4c58831bfa840
Maribel <lwang79@zoho.com>
2022-09-23 17:02:17 -0700

    regex verification addded to the number Q, and updated the rest of the code

--- script.js
+++ script.js
@@ -1,138 +1,123 @@
-// Assignment code here
-FullCharSet = "";
-LowerCharSet = "abcdefghijklmnopqrstuvwxyz";
-SpecCharSet = "~!@#$%^&*()_-+=`:;<>?,./{}[]|";
-UpprCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-NubrCharSet = "1234567890";
-var passwordLength = '';
-var passwordSpecChara = '';
-var passwordCapitChara = '';
-var passwordLowerChara = '';
-var passwordNubrChara = '';
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var promptBtn = document.querySelector("#prompt");
 
-var parameters = function() {
-  // Prompt for length of password minimum of 8, max of 128
-passwordLength = window.prompt("How many characters are in your password? Use numbers only.")
-console.log(passwordLength);
+//parameters asked for here
+const params = () => {
+  paramvalues = [];
 
-// Decides whether we want special characters
-passwordSpecChar = window.prompt("Are there special characters in your password? Please type YES or NO.")
-passwordSpecChara = passwordSpecChar.toUpperCase();
-console.log(passwordSpecChara);
-
-// Decides whether we want capitalized characters
-passwordCapitChar = window.prompt("Are you using CAPITALIZED characters in your password? Please type YES or NO.");
-passwordCapitChara = passwordCapitChar.toUpperCase();
-console.log(passwordCapitChara);
-
-// Decides whether we want lowercase characters
-passwordLowerChar = window.prompt("Are there lower case characters in your password? Please type YES or NO.");
-passwordLowerChara = passwordLowerChar.toUpperCase();
-console.log(passwordLowerChara);
-
-// Decides whether we want numbers
-passwordNubrChar = window.prompt("Are there numbers in your password? Please type YES or NO.");
-passwordNubrChara = passwordNubrChar.toUpperCase();
-console.log(passwordNubrChara);
-}
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  function generatePassword() {
-    var length = passwordLength;
-    FullCharSet = FullCharSet.replace("~!@#$%^&*()_-+=`:;<>?,./{}[]|", '');
-    FullCharSet = FullCharSet.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ", '');
-    FullCharSet = FullCharSet.replace("abcdefghijklmnopqrstuvwxyz", '');
-    FullCharSet = FullCharSet.replace("1234567890", '');
-
-        // Adds Special Characters to the character set
-        if (passwordSpecChara == "YES") {
-        FullCharSet += SpecCharSet;
-        }
-        else if (passwordSpecChara == "NO") {
-          FullCharSet = FullCharSet.replace("~!@#$%^&*()_-+=`:;<>?,./{}[]|", '');
-        }
-        else {
-        FullCharSet += "";
-        }
-
-        // Adds Capitalized Characters to the character set
-        if (passwordCapitChara == "YES") {
-          FullCharSet += UpprCharSet;
-
-        }
-        else if (passwordCapitChara == "NO") {
-          FullCharSet = FullCharSet.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ", '');
-        }
-        else {
-          FullCharSet += "";
-        }
-        
-        // Adds lower case characters to the character set
-        if (passwordLowerChara == "YES") {
-          FullCharSet += LowerCharSet;
-        }
-        else if (passwordLowerChara == "NO") {
-          FullCharSet = FullCharSet.replace("abcdefghijklmnopqrstuvwxyz", '');
-        }
-        else {
-          FullCharSet += "";
-        }
-
-        // Adds numbers to the character set
-        if (passwordNubrChara == "YES") {
-          FullCharSet += NubrCharSet;
-        }
-        else if (passwordNubrChara == "NO") {
-          FullCharSet = FullCharSet.replace("1234567890", '');
-        }
-        else {
-          FullCharSet += "";
-        }
-
-        retVal = "";
-
-        //Guaranteeing that each requirement is accounted for
-        if (passwordSpecChara == "YES") {
-          for (var i = 0, n = SpecCharSet.length; i < 1; ++i) {
-            retVal += SpecCharSet.charAt(Math.floor(Math.random() * n));
-          }
-        }
-        if (passwordCapitChara == "YES") {
-          for (var i = 1, n = UpprCharSet.length; i < 2; ++i) {
-            retVal += UpprCharSet.charAt(Math.floor(Math.random() * n));
-          }
-        }
-        if (passwordLowerChara == "YES") {
-          for (var i = 2, n = LowerCharSet.length; i < 3; ++i) {
-            retVal += LowerCharSet.charAt(Math.floor(Math.random() * n));
-          }
-        }
-        if (passwordNubrChara == "YES") {
-          for (var i = 3, n = NubrCharSet.length; i < 4; ++i) {
-            retVal += NubrCharSet.charAt(Math.floor(Math.random() * n));
-          }
-        }
-        // Generates the password
-        for (var i = 4, n = FullCharSet.length; i < length; ++i) {
-        retVal += FullCharSet.charAt(Math.floor(Math.random() * n));
-        }
-    return retVal;
+  //length
+  lengthQ = window.prompt('How many characters are in your password? Enter a number between 8 and 32, please.');
+  length = Number(lengthQ);
+  if (/([0-9])/.test(lengthQ) == true && 8 < length && length < 32) { // tests to see if the input is true
+      console.log(`Your password will be ${lengthQ} characters long`);
+      paramvalues.push({'length': length});
+  } 
+  else {
+      paramvalues.push({'length': 'invalid input'});
+      throw new Error('invalid input!');
   }
+
+  //spec chara
+  specQ = window.prompt('Are there special characters in your password? Please type YES or NO.');
+  spec = specQ.toUpperCase();
+  if (spec == 'YES') {
+      console.log('You want special characters in your password');
+      paramvalues.push({'spec': true});
+  }
+  else if (spec == 'NO') {
+      console.log('You do not want special characters in your password');
+      paramvalues.push({'spec': false});
+  }
+  else {
+      paramvalues.push({'spec': 'invalid input'});
+      throw new Error('invalid input!');
+  }
+
+  //cap chara
+  capQ = window.prompt('Are there CAPITALIZED characters in your password? Please type YES or NO.');
+  cap = capQ.toUpperCase();
+  if (cap == 'YES') {
+      console.log('You want capitalized characters in your password');
+      paramvalues.push({'cap': true});
+  }
+  else if (cap == 'NO') {
+      console.log('You do not want capitalized characters in your password');
+      paramvalues.push({'cap': false});
+  }
+  else {
+      paramvalues.push({'cap': 'invalid input'});
+      throw new Error('invalid input!');
+  }
+
+  //low chara
+  lowQ = window.prompt('Are there lower case characters in your password? Please type YES or NO.');
+  low = lowQ.toUpperCase();
+  if (low == 'YES') {
+      console.log('You want lower case characters in your password');
+      paramvalues.push({'low': true});
+  }
+  else if (low == 'NO') {
+      console.log('You do not want lower case characters in your password');
+      paramvalues.push({'low': false});
+  }
+  else {
+      paramvalues.push({'low': 'invalid input'});
+      throw new Error('invalid input!');
+  }
+
+  //nums
+  numQ = window.prompt('Are there numbers in your password? Please type YES or NO.');
+  num = numQ.toUpperCase();
+  if (num == 'YES') {
+      console.log('You want numbers in your password');
+      paramvalues.push({'num': true});
+  }
+  else if (num == 'NO') {
+      console.log('You do not want numbers in your password');
+      paramvalues.push({'num': false});
+  }
+  else {
+      paramvalues.push({'num': 'invalid input'});
+      throw new Error('invalid input!');
+  }
+
+  return paramvalues;
+};
+
+//password generating fxn
+const check = () => {
+console.log(paramvalues)
+
+if (paramvalues[0].length == 'invalid input' || paramvalues[1].spec == 'invalid input' || paramvalues[2].cap == 'invalid input' || paramvalues[3].low == 'invalid input' || paramvalues[4].num == 'invalid input') {
+  console.log('Invalid input detected! Please re-enter the requirements again!');
+}
+else if (paramvalues[1].spec == false && paramvalues[2].cap == false && paramvalues[3].low == false && paramvalues[4].num == false) {
+  console.log('No characters available for the password!');
+}
+else {
+  createPassword();
+}
+};
+
+const createPassword = () => {
+  var charset = [];
+  var str = ''
+
+  if (paramvalues[1].spec == true) {
+    charset.push('~!@#$%^&*()_-+=`:;<>?,./{}[]|",');
+  }
+  if (paramvalues[2].cap == true) {
+    charset.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  }
+  if (paramvalues[3].low == true) {}
+  if (paramvalues[4].num == true) {}
+
+  console.log(charset)
 }
 
-parameters();
+params();
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-promptBtn.addEventListener("click", parameters);
+generateBtn.addEventListener("click", check);
+promptBtn.addEventListener("click", params);
